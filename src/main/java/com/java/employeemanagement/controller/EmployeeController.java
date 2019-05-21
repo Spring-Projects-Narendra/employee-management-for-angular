5aaa68bdf8aa83dbe8e2a0282352b012e22c99bb
Narendra Nirala <nknirala.007@gmail.com>
2019-05-21 08:46:53 +0530

    Employee management for Angular 7 application

---
+++ src/main/java/com/java/employeemanagement/controller/EmployeeController.java
@@ -0,0 +1,37 @@
+package com.java.employeemanagement.controller;
+
+import com.java.employeemanagement.model.Employee;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class EmployeeController {
+
+    @GetMapping(value = "/getAllEmployees")
+    public List<Employee> getAllEmployees(){
+        return createList();
+    }
+
+    private static List<Employee> createList() {
+        List<Employee> tempEmployees = new ArrayList<>();
+        Employee emp1 = new Employee();
+        emp1.setName("emp1");
+        emp1.setDesignation("manager");
+        emp1.setEmpId("1");
+        emp1.setSalary(3000);
+
+        Employee emp2 = new Employee();
+        emp2.setName("emp2");
+        emp2.setDesignation("developer");
+        emp2.setEmpId("2");
+        emp2.setSalary(3000);
+        tempEmployees.add(emp1);
+        tempEmployees.add(emp2);
+        return tempEmployees;
+    }
+}
